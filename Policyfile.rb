d7e53edce0483b581ca8d6b5f16058d4459df857
anniehedgpeth <annie.hedgpeth@gmail.com>
2016-09-19 17:20:29 -0500

    cookstyle

--- Policyfile.rb
+++ Policyfile.rb
@@ -1,6 +1,6 @@
-name "ubuntu-server"
-run_list "ubuntu-14-hardening::default"
-default_source :supermarket, "https://supermarket.chef.io"
-cookbook "ubuntu-14-hardening", path: "."
+name 'ubuntu-server'
+run_list 'ubuntu-14-hardening::default'
+default_source :supermarket, 'https://supermarket.chef.io'
+cookbook 'ubuntu-14-hardening', path: '.'
 # cookbook "jenkins", "~> 2.1"
 # cookbook "mysql", github: "chef-cookbooks/mysql", branch: "master"
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,18 +5,18 @@
 # Copyright (c) 2016 Annie Hedgpeth, All Rights Reserved.
 
 ['/boot/grub/grub.cfg'].each do |name|
-   file name do
-     owner 'root'
-     group 'root'
-     mode '0700'
-   end
- end
+  file name do
+    owner 'root'
+    group 'root'
+    mode '0700'
+  end
+end
 
-files={
-  'limits.conf'=> '/etc/security/limits.conf',
-  'sysctl.conf'=> '/etc/sysctl.conf'
+files = {
+  'limits.conf' => '/etc/security/limits.conf',
+  'sysctl.conf' => '/etc/sysctl.conf'
 }
-files.each do |key,value|
+files.each do |key, value|
   cookbook_file value do
     source key
     action :create
@@ -28,13 +28,13 @@ execute 'unlink' do
   only_if 'dpkg -s prelink'
 end
 
-['apport','whoopsie','prelink','nis'].each do |name|
-   package name do
-     action :purge
-   end
- end
+%w(apport whoopsie prelink nis).each do |name|
+  package name do
+    action :purge
+  end
+end
 
-# Commented out because, while the CIS benchmark recommends it be activated, Chef cannot be run if Apparmor is enabled. 
+# Commented out because, while the CIS benchmark recommends it be activated, Chef cannot be run if Apparmor is enabled.
 # service ['apparmor', 'apparmor-utils'] do
 #   action :enable
 # end
